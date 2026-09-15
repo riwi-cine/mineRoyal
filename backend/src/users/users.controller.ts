47bee9b14eb1df70a1536eafef8936bc079a7ed4
leonela miranda <leonelamiranda141024@gmail.com>
2026-09-15 12:47:45 -0500

    fe: users-initial-crud

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { get } from 'node:http';
+import { UsersService } from './users.service'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor (
+        private readonly usersService: UsersService,
+    ) {}
+
+    @Get()
+    getUsers() {
+        return this.usersService.findAll()
+    }
+
+    @Get(':id') 
+    findOne (@Param('id') id: string){
+        return this.usersService.findOne(id)
+    }
+
+    @Post()
+    CreateUsers (@Body() body: CreateUserDto) {
+        return this.usersService.create(body)
+    }
+};
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    findAll() {
+        return [
+            {
+                id: 1, name: 'efrain,'
+            }
+        ]
+    }
+
+    findOne(id: string) {
+        return [
+            {
+                id: 2, name: 'pepe'
+            }
+        ]
+    }
+
+    create(body: CreateUserDto) {
+        return [
+            {
+                id: 3, name: 'pepe2', email: 'hlaluz59@gmail.com', password: 'jeje123'
+            }
+        ]
+    }
+}
